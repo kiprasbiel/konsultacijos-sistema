4ee31ae9064a939f3d895c41d080f4ab577be902
Kipras <info@anaweb.lt>
2019-12-19 14:52:10 +0200

    Nauja el. pasto konfiguracija mail.php faile

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'rapsas.serveriai.lt'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'SSL'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('sistema@vsipromas.lt'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('TavienaiTavienai'),
 
     /*
     |--------------------------------------------------------------------------
