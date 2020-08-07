a1c5575b884300fbf275cb5815dad25aa450ea31
Kipras <reg@anaweb.lt>
2020-08-07 18:23:32 +0300

    Reformating Consultation model

--- app/Consultation.php
+++ app/Consultation.php
@@ -3,42 +3,57 @@
 namespace App;
 
 use App\CustomClasses\ConsultationsClass;
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
+use DateTime;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 class Consultation extends Model
 {
-
-//    protected $fillable = [
-//        'client_id',
-//    ];
-
     /**
-     * Return Consultations break length in minutes
      * @return \DateInterval|false
+     * @throws Exception
      */
     public function break_len(){
-        $con_class = new ConsultationsClass();
-        return $con_class->break_len_calc($this->break_start, $this->break_end);
+        $break_raw = Consultation_meta::where('consultation_id', $this->id)->where('type', 'consultation_break')->pluck('value');
+        $break_json = json_decode($break_raw[0]);
+        $diff_date = new DateTime('00:00');
+        $ref_date = clone $diff_date;
+        foreach ($break_json as $single_break){
+            $start_time = new DateTime($single_break->break_start);
+            $end_time = new DateTime($single_break->break_end);
+            $diff_date->add($start_time->diff($end_time));
+        }
+        return $ref_date->diff($diff_date);
     }
 
     /**
      * Returns actual consultation length
      * including breaks
      * @return string
+     * @throws Exception
      */
     public function con_actual_len(){
-        $con_class = new ConsultationsClass();
-        return $con_class->actual_con_length($this->break_len(), $this->consultation_length);
+        $con_len_time = new DateTime($this->consultation_length);
+        return $con_len_time->add($this->break_len())->format('H:i');
     }
 
     /**
      * Returns consultation end datetime
      * including breaks
-     * @return \Carbon\Carbon
+     * @return Carbon
+     * @throws Exception
      */
     public function con_end_datetime(){
-        $con_class = new ConsultationsClass();
-        return $con_class->con_end_datetime($this->consultation_time, $this->consultation_date, $this->con_actual_len());
+        if (substr_count($this->consultation_time, ':') == 1){
+            $con_start_date = Carbon::createFromFormat('Y-m-d H:i', $this->consultation_date . ' ' . $this->consultation_time);
+        }
+        else{
+            $con_start_date = Carbon::createFromFormat('Y-m-d H:i:s', $this->consultation_date . ' ' . $this->consultation_time);
+        }
+        $con_len_time = CarbonInterval::createFromFormat('H:i', $this->con_actual_len());
+        return $con_start_date->add($con_len_time);
     }
 
     /**
@@ -47,10 +62,19 @@ class Consultation extends Model
      * true for passed
      * false for to be still
      * @return bool
+     * @throws Exception
      */
     public function is_con_over(){
-        $con_class = new ConsultationsClass();
-        return $con_class->is_con_over($this->con_end_datetime());
+//        $con_class = new ConsultationsClass();
+//        return $con_class->is_con_over($this->con_end_datetime());
+
+        $now = Carbon::now('Europe/Vilnius');
+        if ($this->con_end_datetime()->greaterThan($now)){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
     public function client(){
--- app/CustomClasses/ConsultationsClass.php
+++ app/CustomClasses/ConsultationsClass.php
@@ -10,35 +10,35 @@ use DateTime;
 
 class ConsultationsClass
 {
-    public function break_len_calc($start, $end){
-        $start_time = new DateTime($start);
-        $end_time = new DateTime($end);
-        return $start_time->diff($end_time);
-    }
+//    public function break_len_calc($start, $end){
+//        $start_time = new DateTime($start);
+//        $end_time = new DateTime($end);
+//        return $start_time->diff($end_time);
+//    }
 
-    public function actual_con_length($break_len, $con_len){
-        $con_len_time = new DateTime($con_len);
-        return $con_len_time->add($break_len)->format('H:i');
-    }
+//    public function actual_con_length($break_len, $con_len){
+//        $con_len_time = new DateTime($con_len);
+//        return $con_len_time->add($break_len)->format('H:i');
+//    }
 
-    public function con_end_datetime($con_start_time, $con_start_date, $con_len){
-        if (substr_count($con_start_time, ':') == 1){
-            $con_start_date = Carbon::createFromFormat('Y-m-d H:i', $con_start_date . ' ' . $con_start_time);
-        }
-        else{
-            $con_start_date = Carbon::createFromFormat('Y-m-d H:i:s', $con_start_date . ' ' . $con_start_time);
-        }
-        $con_len_time = CarbonInterval::createFromFormat('H:i', $con_len);
-        return $con_start_date->add($con_len_time);
-    }
+//    public function con_end_datetime($con_start_time, $con_start_date, $con_len){
+//        if (substr_count($con_start_time, ':') == 1){
+//            $con_start_date = Carbon::createFromFormat('Y-m-d H:i', $con_start_date . ' ' . $con_start_time);
+//        }
+//        else{
+//            $con_start_date = Carbon::createFromFormat('Y-m-d H:i:s', $con_start_date . ' ' . $con_start_time);
+//        }
+//        $con_len_time = CarbonInterval::createFromFormat('H:i', $con_len);
+//        return $con_start_date->add($con_len_time);
+//    }
 
-    public function is_con_over($con_end){
-        $now = Carbon::now('Europe/Vilnius');
-        if ($con_end->greaterThan($now)){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
+//    public function is_con_over($con_end){
+//        $now = Carbon::now('Europe/Vilnius');
+//        if ($con_end->greaterThan($now)){
+//            return false;
+//        }
+//        else{
+//            return true;
+//        }
+//    }
 }
